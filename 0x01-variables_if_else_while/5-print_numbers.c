76ffc2f70b80cdabc60a25aa12877bf6620cade2
mouhcine wildane <muhssenwildane@gmail.com>
2023-08-11 16:57:37 -0700

    5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-    int digit = 0;
+	int digit = 0;
 
-    while (digit < 10)
-    {
-        putchar(digit + '0'); // Convert digit to character and print
-        digit++;
-    }
+	while (digit < 10)
+	{
+		putchar(digit + '0'); 
+		digit++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
 
