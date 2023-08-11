1637d51b71d028813b8116e09913dfa7321d128a
mouhcine wildane <muhssenwildane@gmail.com>
2023-08-11 15:54:10 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints the lowercase alphabet followed by a new line using putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
