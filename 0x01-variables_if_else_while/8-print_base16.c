65c80f0f93d2134c4e96e3190f6176aa287ec563
mouhcine wildane <muhssenwildane@gmail.com>
2023-08-11 17:16:26 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints number a new line using putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int number;
+
+    for (number = 0; number < 10; number++)
+    {
+        putchar(number + '0');
+    }
+
+    for (number = 'a'; number <= 'f'; number++)
+    {
+        putchar(number);
+    }
+
+    putchar('\n');
+
+    return (0);
+}
