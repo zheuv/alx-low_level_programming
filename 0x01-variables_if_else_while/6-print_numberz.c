f686171c13d133aca123904b83a57ccaf30b5ebc
mouhcine wildane <muhssenwildane@gmail.com>
2023-08-11 17:04:12 -0700

    5

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Descri base 10 followed by a new line using putchar.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int digit = 0;
+
+        while (digit < 10)
+        {
+                putchar(digit + '0');
+                digit++;
+        }
+
+        putchar('\n');
+
+        return (0);
+}
