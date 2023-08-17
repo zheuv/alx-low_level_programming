7d9b3d7d4ff6c85f9d4fe540642f8ccb777ce168
mouhcine wildane <muhssenwildane@gmail.com>
2023-08-17 13:02:00 -0700

    8

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers from 1 to 100 with specific conditions
+ *              applied to each number.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		}
+		else if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
+
