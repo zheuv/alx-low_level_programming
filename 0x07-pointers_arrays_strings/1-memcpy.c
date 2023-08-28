fcb317f532e398c3a01f05058480dc148207be42
zheuv <muhssenwildane@gmail.com>
2023-08-28 23:13:30 +0100

    1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -10,11 +10,13 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
+	int i = n;
 
-	for (; r < n; r++)
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
 	}
 	return (dest);
 }
+
