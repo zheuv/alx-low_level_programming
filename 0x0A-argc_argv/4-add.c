5a1967772035e7540fd8917863d1388c209c1960
zheuv <muhssenwildane@gmail.com>
2023-09-02 00:51:23 +0100

    4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,12 +10,14 @@
  */
 int check_num(char *str)
 {
+	/*Declaring variables*/
 	unsigned int count;
 
 	count = 0;
-	while (count < strlen(str))
+	while (count < strlen(str)) /*count string*/
+
 	{
-		if (!isdigit(str[count]))
+		if (!isdigit(str[count])) /*check if str there are digit*/
 		{
 			return (0);
 		}
@@ -34,20 +36,25 @@ int check_num(char *str)
  */
 
 int main(int argc, char *argv[])
-{	
+
+{
+
+	/*Declaring variables*/
 	int count;
 	int str_to_int;
 	int sum = 0;
 
 	count = 1;
-	while (count < argc)
+	while (count < argc) /*Goes through the whole array*/
 	{
 		if (check_num(argv[count]))
+
 		{
-			str_to_int = atoi(argv[count]);
+			str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 			sum += str_to_int;
 		}
 
+		/*Condition if one of the number contains symbols that are not digits*/
 		else
 		{
 			printf("Error\n");
@@ -57,7 +64,7 @@ int main(int argc, char *argv[])
 		count++;
 	}
 
-	printf("%d\n", sum);
+	printf("%d\n", sum); /*print sum*/
 
 	return (0);
 }
