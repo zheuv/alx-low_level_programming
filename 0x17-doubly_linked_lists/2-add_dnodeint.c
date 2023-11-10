f809101600eaf32c39f1a66b270e036803d0aa8d
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-10 20:53:40 +0100

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,27 @@
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *current = *head;
+	dlistint_t *new;
+
+	new = malloc(sizeof(dlistint_t));
+	
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (current == NULL)
+		*head = new
+		return(new);
+	
+	new->next = *head;
+	(*head)->prev = new;
+	*head = new;
+
+	return(new);
+}
+
