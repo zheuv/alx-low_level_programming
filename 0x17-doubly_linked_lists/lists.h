01ad76e7a9834643609bfe069ce589416e271f1e
mouhcine wildane <muhssenwildane@gmail.com>
2023-11-10 13:24:15 -0800

    c

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -28,4 +28,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-#endif/*LIST_H
+#endif/*LIST_H*/
