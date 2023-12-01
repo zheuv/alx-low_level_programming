8d4bbcd22be3ebc8f9d66d8f5592e3a5f2c50c1f
mouhcine wildane <muhssenwildane@gmail.com>
2023-12-01 17:33:46 +0100

    4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key in the hash table.
+ * @ht: The hash table.
+ * @key: The key to look up.
+ *
+ * Return: The value associated with the key, or NULL if the key is not found.
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	if ( ht == NULL || ht->size == 0 || ht->array == NULL || key == NULL || strlen(key) == 0 )
+		return NULL;
+	unsigned long int index;
+	index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *current;
+
+	current = ht->array[index];
+	while current != NULL
+	{
+		if (strcmp(current->key, key) == 0)
+                {
+			return current->value;
+                }
+                if (current->next == NULL)
+                        return NULL;
+                current = current->next;
+        }
+
+}
