18367f66099aff525ff3fc56cdd3ceb082207cb8
mouhcine wildane <muhssenwildane@gmail.com>
2023-12-01 17:26:04 +0100

    4

--- 0x1A-hash_tables/4-hash_table_set.c
+++ 0x1A-hash_tables/4-hash_table_set.c
@@ -1,35 +1,27 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - Retrieves a value associated with a key in the hash table.
- * @ht: The hash table.
- * @key: The key to look up.
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: table to retrieve value from
+ * @key: key to find value
  *
- * Return: The value associated with the key, or NULL if the key is not found.
+ * Return: value associated with key, or NULL if key cannot be found
  */
-
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	if ( ht == NULL || ht->size == 0 || ht->array == NULL || key == NULL || strlen(key) == 0 )
-		return NULL;
 	unsigned long int index;
-	index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current;
+	hash_node_t *tmp;
 
-	current = ht->array[index];
-	if (current == NULL || current->key == NULL || current->value == NULL)
-		return NULL;
-	while current != NULL
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(current->key, key) == 0)
-		{
-		        if (current->value == NULL)
-				return NULL;
-			return current->value;
-		}
-		if (current->next == NULL)
-			return NULL;
-		current = current->next;
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
 	}
-	return NULL;
-}	
+	return (NULL);
+}
