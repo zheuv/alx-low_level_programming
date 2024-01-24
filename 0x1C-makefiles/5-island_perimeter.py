7a85f63a289e32ed9269b77b3a2b667b5b64c6c9
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-24 20:57:16 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
 """ technical interview task """
+
+
 def island_perimeter(grid):
     """ calculates the perimeter of the island """
     p = 0
     flag = 0
     for j in range(len(grid)):
-        for i in range (len(grid[j])):
+        for i in range(len(grid[j])):
             if grid[j][i] == 1:
                 flag = 1
                 if j == 0:
                     p += 2
                 else:
                     if i == 0:
-                        p +=2
+                        p += 2
                     elif grid[j-1][i] != 1 or grid[j][i-1] != 1:
-                        p +=2
+                        p += 2
     if flag == 1:
         p += 2
     return(p)
