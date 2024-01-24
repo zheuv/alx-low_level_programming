c43676ac0118b3a2ef3900b201b92f1915b66214
Mouhcine Wildane <117749214+zheuv@users.noreply.github.com>
2024-01-25 00:11:36 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,17 +10,14 @@ def island_perimeter(grid):
         for i in range(len(grid[j])):
             if grid[j][i] == 1:
                 flag = 1
-                if j == 0:
+                if (j == 0) or (i == 0):
                     p += 2
-                else:
-                    if i == 0:
-                        p += 2
-                    elif not(
-                        grid[j-1][i] == 1 and
-                        grid[j][i-1] == 1 and
-                        grid[j-1][i-1] == 1
-                    ):
-                        p += 2
+                elif not(
+                    grid[j-1][i] == 1 and
+                    grid[j][i-1] == 1 and
+                    grid[j-1][i-1] == 1
+                ):
+                    p += 2
     if flag == 1:
         p += 2
     return(p)
