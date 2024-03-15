413f17e5cd6f4e0cff36ab576c3b8164ad958ec7
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-15 09:47:19 -0700

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,16 +1,31 @@
 #include "search_algos.h"
 
-
+/**
+ * print_array - Prints the elements of an array within a specified range.
+ *
+ * @array: Pointer to the first element of the array.
+ * @begin: Index of the first element to print.
+ * @end: Index of the last element to print.
+ */
 void print_array( int *array, size_t beguin, size_t end)
 {
 	while (beguin < end)
 	{
 		printf("%d, ", array[beguin]);
-		beguin++
+		beguin++;
 	}
 	printf("%d\n", array[end]);
 }
 
+/**
+ * binary_search - Searches for a value in a sorted array using the binary search algorithm.
+ *
+ * @array: Pointer to the first element of the sorted array.
+ * @size: Number of elements in the array.
+ * @value: Value to search for in the array.
+ *
+ * Return: Index of the value if found, or -1 if the value is not present in the array.
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	size_t  cursor;
