0afd18b484ef67d04d7eee41c89e099fbb55b17c
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-15 01:08:39 +0000

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -3,14 +3,18 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
+	int j;
+	j = 0;
+
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%d] = [%d]", j, array[i]);
 		if (value == array[i])
 		{
-			return (i);
+			return (j);
 		}
+		j++;
 	}
 	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-#include <stdio.h>  // Include necessary libraries
+#include <stdio.h>
 
 /* Function prototype for linear search */
 int linear_search(int *array, size_t size, int value);
