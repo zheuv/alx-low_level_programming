53abeec4318544b7a45918762118e75371e519bb
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-14 18:26:23 -0700

    0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,7 +9,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", j, array[i]);
+		printf("Value checked array[%d] = [%d]\n", j, array[i]);
 		if (value == array[i])
 		{
 			return (j);
