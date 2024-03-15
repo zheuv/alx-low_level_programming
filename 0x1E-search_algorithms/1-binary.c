2f06026b56e1f4d74ae87982b23fdd7565a229be
mouhcine wildane <muhssenwildane@gmail.com>
2024-03-15 16:40:28 +0000

    1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,7 +1,15 @@
 #include "search_algos.h"
 
 
-
+void print_array( int *array, size_t beguin, size_t end)
+{
+	while (beguin < end)
+	{
+		printf("%d, ", array[beguin]);
+		beguin++
+	}
+	printf("%d\n", array[end]);
+}
 
 int binary_search(int *array, size_t size, int value)
 {
@@ -14,15 +22,16 @@ int binary_search(int *array, size_t size, int value)
 	while (beguin <= end) 
 	{
 		cursor = (end + beguin) / 2;
+		print_array(array, beguin, end);
 		if (array[cursor] > value)
 		{
 			end = cursor - 1;
 		}
-		else if ( array[cursor] < value)
+		else if (array[cursor] < value)
 		{	
 			beguin = cursor + 1;
 		}
-		else 
+		else
 		{
 				return cursor;
 		}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,6 @@
 /* Function prototype for linear search */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t beguin, size_t end);
 #endif /* SEARCH_ALGOS_H */
 
